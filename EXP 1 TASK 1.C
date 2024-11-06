0ca6ee8621dae2b6b710cd90aa489f861e2efb45
sanchi111 <sanchi@eng.rizvi.edu.in>
2024-11-06 14:55:20 +0530

    Create EXP 1 TASK 1.C

---
+++ EXP 1 TASK 1.C
@@ -0,0 +1,19 @@
+/*
+    Expt 1 - Task 1
+    Module 1 : Fundamentals of C Programming
+    Aim: WAP to print your name, age, class, division  
+        and UIN on the screen. Use escape sequences 
+        to properly format the output.
+    
+*/
+
+#include<stdio.h>
+int main(){
+    printf("Full Name\t: Sanjay Singh\n");
+    printf("Age\t\t: 26\n");
+    printf("Class\t\t: FE\n");
+    printf("Division\t: D\n");
+    printf("UIN\t\t: 241P005\n");
+
+    return 0;
+}
